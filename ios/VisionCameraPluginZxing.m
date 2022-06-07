9a2166f9b4626d73184e780d821a9254fee5e4a3
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-06-07 15:10:07 +0900

    App.tsx

--- ios/VisionCameraPluginZxing.m
+++ ios/VisionCameraPluginZxing.m
@@ -1,16 +1,27 @@
 #import "VisionCameraPluginZxing.h"
+#import <VisionCamera/Frame.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
 
 @implementation VisionCameraPluginZxing
 
 RCT_EXPORT_MODULE()
 
+static inline id scanBarcodes(Frame *frame, NSArray *args) {
+  CMSampleBufferRef buffer = frame.buffer;
+  UIImageOrientation orientation = frame.orientation;
+  // code goes here
+  return @[];
+}
+
+VISION_EXPORT_FRAME_PROCESSOR(scanBarcodes)
+
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
+RCT_REMAP_METHOD(multiply, multiplyWithA
+                 : (nonnull NSNumber *)a withB
+                 : (nonnull NSNumber *)b withResolver
+                 : (RCTPromiseResolveBlock)resolve withRejecter
+                 : (RCTPromiseRejectBlock)reject) {
   NSNumber *result = @([a floatValue] * [b floatValue]);
 
   resolve(result);
