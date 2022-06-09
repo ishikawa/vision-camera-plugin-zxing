a003a817b28be54a7aa849c5910bd7b45acfed4b
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-06-09 09:47:18 +0900

    fix format script

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,11 @@
 import 'react-native-reanimated';
 import React, { useEffect, useState } from 'react';
 import { Alert, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 import { scanBarCodes } from 'vision-camera-plugin-zxing';
 
 const App: React.FC = () => {
