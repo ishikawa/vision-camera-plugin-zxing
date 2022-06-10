801f67f41c02b17ed0c8718999e992d0d6d9fcd9
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-06-10 18:37:42 +0900

    add animation

--- example/App.tsx
+++ example/App.tsx
@@ -1,28 +1,34 @@
 import 'react-native-reanimated';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
-  View,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   LayoutRectangle,
+  StyleProp,
+  ViewStyle,
 } from 'react-native';
 import {
   Camera,
   useCameraDevices,
   useFrameProcessor,
 } from 'react-native-vision-camera';
-import { runOnJS } from 'react-native-reanimated';
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+  useSharedValue,
+} from 'react-native-reanimated';
 import { scanBarCodes, ScanResult } from 'vision-camera-plugin-zxing';
 
 const App: React.FC = () => {
   const cameraDevices = useCameraDevices();
   const cameraDevice = cameraDevices.back;
   const [hasCameraPermission, setHasCameraPermission] = useState(false);
-  const [, setScanResult] = useState<ScanResult | null>(null);
-  const [codeRect, setCodeRect] = useState<LayoutRectangle | null>(null);
-  const [cameraRect, setCameraRect] = useState<LayoutRectangle | null>(null);
+
+  // Animation values
+  const cameraRect = useSharedValue<LayoutRectangle | null>(null);
+  const scanResult = useSharedValue<ScanResult | null>(null);
 
   // Camera permission
   useEffect(() => {
@@ -57,79 +63,72 @@ const App: React.FC = () => {
     })();
   }, []);
 
-  const onScanBarCode = useCallback(
-    (scanResult: ScanResult) => {
-      console.log('scanResult =', scanResult, 'cameraRect =', cameraRect);
-      setScanResult(scanResult);
+  // uses 'scanResult' to position the rectangle on screen.
+  // smoothly updates on UI thread whenever 'scanResult' is changed
+  const boxOverlayStyle: StyleProp<ViewStyle> = useAnimatedStyle(() => {
+    if (cameraRect.value && scanResult.value?.code?.points.length === 4) {
+      const points = scanResult.value.code.points;
 
-      if (cameraRect && scanResult.code?.points.length === 4) {
-        const points = scanResult.code.points;
+      const scaleX = cameraRect.value.width / scanResult.value.width;
+      const scaleY = cameraRect.value.height / scanResult.value.height;
 
-        const scaleX = cameraRect.width / scanResult.width;
-        const scaleY = cameraRect.height / scanResult.height;
+      const minX = Math.min(...points.map((pt) => pt.x)) * scaleX;
+      const minY = Math.min(...points.map((pt) => pt.y)) * scaleY;
+      const maxX = Math.max(...points.map((pt) => pt.x)) * scaleX;
+      const maxY = Math.max(...points.map((pt) => pt.y)) * scaleY;
+      const bounds = {
+        x: minX,
+        y: minY,
+        width: maxX - minX,
+        height: maxY - minY,
+      };
+      const animationOptions = {
+        duration: 100,
+      };
 
-        const minX = Math.min(...points.map((pt) => pt.x)) * scaleX;
-        const minY = Math.min(...points.map((pt) => pt.y)) * scaleY;
-        const maxX = Math.max(...points.map((pt) => pt.x)) * scaleX;
-        const maxY = Math.max(...points.map((pt) => pt.y)) * scaleY;
+      return {
+        position: 'absolute',
+        borderWidth: 2,
+        borderColor: 'red',
+        left: withTiming(bounds.x, animationOptions),
+        top: withTiming(bounds.y, animationOptions),
+        width: bounds.width,
+        height: bounds.height,
+      };
+    } else {
+      return {
+        visibility: 'hidden',
+      };
+    }
+  });
 
-        setCodeRect({
-          x: minX,
-          y: minY,
-          width: maxX - minX,
-          height: maxY - minY,
-        });
-      } else {
-        setCodeRect(null);
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet';
+    const value = scanBarCodes(frame, []);
+
+    if (value) {
+      if (value.base64JPEG) {
+        console.log(value);
       }
-    },
-    [cameraRect]
-  );
-
-  const frameProcessor = useFrameProcessor(
-    (frame) => {
-      'worklet';
-      const value = scanBarCodes(frame, []);
-
-      if (value) {
-        if (value.base64JPEG) {
-          console.log(value);
-        }
-        if (value.code) {
-          runOnJS(onScanBarCode)(value);
-        }
+      if (value.code) {
+        scanResult.value = value;
       }
-    },
-    [onScanBarCode]
-  );
+    }
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       {cameraDevice && hasCameraPermission ? (
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
         <Camera
           frameProcessor={frameProcessor}
           style={[styles.camera]}
           device={cameraDevice}
           isActive={true}
           onLayout={({ nativeEvent: { layout } }) => {
-            setCameraRect(layout);
+            cameraRect.value = layout;
           }}
         >
-          {codeRect && (
-            <View
-              style={{
-                position: 'absolute',
-                borderWidth: 2,
-                borderColor: 'red',
-                left: codeRect.x,
-                top: codeRect.y,
-                width: codeRect.width,
-                height: codeRect.height,
-              }}
-            />
-          )}
+          <Animated.View style={boxOverlayStyle} />
         </Camera>
       ) : null}
       <StatusBar barStyle="default" />
